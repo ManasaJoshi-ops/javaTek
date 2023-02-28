4fa100a3c46e06ad43cc4637bc9237629917b923
Manasa Joshi <manjoshi@BLR.ALLEGISINDIA.COM>
2023-03-01 02:17:08 +0530

    switch programs added

--- javaproject/src/javaproject/loops.java
+++ javaproject/src/javaproject/loops.java
@@ -38,13 +38,13 @@ public class loops {
 //			}
 //			System.out.println(i);
 //		}
-		for(int i=1;i<=10;i++) {
-			System.out.println(i);
-			if(i>5) {
-				continue;
-			}
-			
-		}
+//		for(int i=1;i<=10;i++) {
+//			System.out.println(i);
+//			if(i>5) {
+//				continue;
+//			}
+//			
+//		}
 		
 		//while loop
 		int i=1;
---
+++ javaproject/src/javaproject/switchFunction.java
@@ -0,0 +1,23 @@
+package javaproject;
+
+public class switchFunction {
+	public static void main(String args[]) {
+		int choice=3;
+		switch(choice) {
+		case 1:
+			System.out.println("Main menu");
+			break;
+		case 2:
+			System.out.println("Exit Screen");
+			break;
+		case 3:
+			System.out.println("Restart");
+			break;	
+		default:
+			System.out.println("Please select the available options");
+			
+			
+		}
+	}
+
+}
