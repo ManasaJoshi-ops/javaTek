5efd5152a8193e9b74fa33269441460cc169f919
Manasa Joshi <manjoshi@BLR.ALLEGISINDIA.COM>
2023-02-28 17:14:12 +0530

    datatypes program is added

---
+++ javaproject/src/javaproject/datatypesdemo.java
@@ -0,0 +1,28 @@
+package javaproject;
+
+public class datatypesdemo {
+	static byte a =50;
+	public static void main(String[] args) {
+		System.out.println(a);
+		int a =150;
+	    System.out.println(a);
+		int c=2;
+		int x=c;
+		System.out.println(x);
+		x=x+3;
+		System.out.println(x);
+		System.out.println(c);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		float f=1.233f;
+		System.out.println(f);
+		double g=1.5555666;
+		System.out.println(g);
+	}
+
+}
