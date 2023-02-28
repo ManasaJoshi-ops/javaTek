4bf5404373ca49ced80524f301b8da972722afef
Manasa Joshi <manjoshi@BLR.ALLEGISINDIA.COM>
2023-02-28 12:11:32 +0530

    nonStaticmethods Added

--- javaproject/src/javaproject/nonStaticMethods.java
+++ javaproject/src/javaproject/nonStaticMethods.java
@@ -1,8 +1,15 @@
 package javaproject;
 
 public class nonStaticMethods {
+	public nonStaticMethods() {
+		System.out.println("From constructor");
+	}
+	{
+		System.out.println("from non static block");
+	}
 	public static void main(String args[]) {
 		System.out.println("main method");
+		nonStaticMethods obj1=new nonStaticMethods();
 	}
 
 }
