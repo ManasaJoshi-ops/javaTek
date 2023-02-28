ac228777306fc8cb1af4f160410281379db408f8
Manasa Joshi <manjoshi@BLR.ALLEGISINDIA.COM>
2023-02-28 11:38:35 +0530

    first program added

---
+++ javaproject/src/javaproject/democlass.java
@@ -0,0 +1,8 @@
+package javaproject;
+
+public class democlass {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ javaproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author manjoshi
+ *
+ */
+module javaproject {
+}
