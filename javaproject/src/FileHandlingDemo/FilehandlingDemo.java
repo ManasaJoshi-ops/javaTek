0a30af6724fe28161d1aaa1808c1107061a7010d
Manasa Joshi <manjoshi@BLR.ALLEGISINDIA.COM>
2023-03-05 05:05:02 +0530

    new programs related to filehandling

--- javaproject/src/FileHandlingDemo/FilehandlingDemo.java
+++ javaproject/src/FileHandlingDemo/FilehandlingDemo.java
@@ -3,13 +3,17 @@ package FileHandlingDemo;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class FilehandlingDemo {
 public static void main(String[] args) {
 //	FileInputStream();
-	FileOutputStream();
+//	FileOutputStream();
+//	FileReader();
+//	FileWriter();
+	BufferedReader();
 }
 static void FileInputStream() {
 	File hello=new File("C:/Users/manjoshi/Desktop/hello.txt");
@@ -40,10 +44,63 @@ static void FileOutputStream() {
 			String data = "good morning";
 			byte[] dataByteArray = data.getBytes();
 			fos.write(dataByteArray);
+			fos.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
+	} catch (FileNotFoundException e) {
+		System.out.println("Exception occured"+e.getMessage());
+	}
+}
+static void FileReader() {
+	File hello=new File("C:/Users/manjoshi/Desktop/hello.txt");
+	try {
+		java.io.FileReader fr=new java.io.FileReader(hello);
+		System.out.println("File opened");
+		System.out.println("------");
+		int i;
+		try {
+			while((i=fr.read())!=-1) {
+				System.out.print((char)i);
+			}fr.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	} catch (FileNotFoundException e) {
+		System.out.println("Exception occured"+e.getMessage());
+	}
+}
+static void FileWriter() {
+	File hello=new File("C:/Users/manjoshi/Desktop/hello.txt");
+	try {
+		java.io.FileWriter fw=new FileWriter(hello);
+		System.out.println("File opened");
+		System.out.println("------");
+		    fw.write("using file writer\n");
+		    fw.write("Another line");
+			fw.close();
+		} catch (FileNotFoundException e) {
+		System.out.println("Exception occured"+e.getMessage());
+	}catch (IOException e) {
+	e.printStackTrace();
+}
+}
+static void BufferedReader() {
+	File hello=new File("C:/Users/manjoshi/Desktop/hello.txt");
+	try {
+		java.io.FileReader fr=new java.io.FileReader(hello);
+		java.io.BufferedReader br=new java.io.BufferedReader(fr);
+		System.out.println("File opened");
+		System.out.println("------");
+		String line;;
+		try {
+			while((line=br.readLine())!=null) {
+				System.out.println(line);
+			}fr.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	} catch (FileNotFoundException e) {
 		System.out.println("Exception occured"+e.getMessage());
 	}
